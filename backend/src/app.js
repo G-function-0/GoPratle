250dac85189551abef6127a25f82e35ffa750b56
Ujjawal Verma <ujjawalverma221@gmail.com>
2026-01-31 05:32:15 +0530

    Implement CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,13 +2,15 @@ import express from "express";
 import { router } from "./routes/requirements.routes.js";
 import { config } from "./config/index.js";
 import { connectDB } from "./db/index.js";
-
+import cors from "cors";
 await connectDB();
 
 const PORT = config.port;
 const app = express();
 
-
+app.use(cors({
+    origin : ["http://localhost:3000",],
+}));
 app.use(express.json());
 app.use("/api",router);
 
